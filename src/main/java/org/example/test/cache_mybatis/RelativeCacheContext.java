16afa97a55973310daca5fd0e4580d8c3f9b00c7
liaowen <liaowen2013win@163.com>
2022-02-07 10:20:21 +0800

    升级pom依赖

--- src/main/java/org/example/test/cache_mybatis/RelativeCacheContext.java
+++ src/main/java/org/example/test/cache_mybatis/RelativeCacheContext.java
@@ -12,13 +12,19 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class RelativeCacheContext {
 
-    // 存储全量缓存的映射关系
+    /**
+     * 存储全量缓存的映射关系
+     */
     public static final Map<Class<?>, MyRelativeCache> MAPPER_CACHE_MAP = new ConcurrentHashMap<>();
-    // 存储 Mapper 对应缓存 需要to更新缓存，但是此时 Mapper 对应缓存还未加载
-    // 也就是 Class<?> 对应的缓存更新时，需要更新 List<RelativeCache> 中的缓存
+    /**
+     * 存储 Mapper 对应缓存 需要to更新缓存，但是此时 Mapper 对应缓存还未加载
+     * 也就是 Class<?> 对应的缓存更新时，需要更新 List<RelativeCache> 中的缓存
+     */
     public static final Map<Class<?>, List<MyRelativeCache>> UN_LOAD_TO_RELATIVE_CACHES_MAP = new ConcurrentHashMap<>();
-    // 存储 Mapper 对应缓存 需要from更新缓存，但是在 加载 Mapper 缓存时，这些缓存还未加载
-    // 也就是 List<RelativeCache> 中的缓存更新时，需要更新 Class<?> 对应的缓存
+    /**
+     * 存储 Mapper 对应缓存 需要from更新缓存，但是在 加载 Mapper 缓存时，这些缓存还未加载
+     * 也就是 List<RelativeCache> 中的缓存更新时，需要更新 Class<?> 对应的缓存
+     */
     public static final Map<Class<?>, List<MyRelativeCache>> UN_LOAD_FROM_RELATIVE_CACHES_MAP = new ConcurrentHashMap<>();
 
     public static void putCache(Class<?> clazz, MyRelativeCache cache) {
