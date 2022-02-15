74d161970bc10caeb0942873af648053ed35cd33
liaowen <liaowen2013win@163.com>
2022-02-15 14:16:31 +0800

    修改user

--- src/main/java/org/example/test/model/entity/UserEntity.java
+++ src/main/java/org/example/test/model/entity/UserEntity.java
@@ -2,9 +2,14 @@ package org.example.test.model.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.example.test.annotations.CacheField;
+import org.example.test.annotations.CacheKey;
 
 import java.io.Serializable;
 
@@ -16,6 +21,11 @@ import java.io.Serializable;
  */
 @EqualsAndHashCode(callSuper = true)
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName("tb_user")
+@CacheKey(name = "userId")
+@CacheField(name = "userId")
 public class UserEntity extends Model<UserEntity> implements Serializable {
 
     private static final long serialVersionUID = 6184875766403113480L;
